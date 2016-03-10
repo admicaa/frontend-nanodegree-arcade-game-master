061c11c5711fa9eb16ba28f4eeb6048a5c11c127
topplethepat <lenore.alford@gmail.com>
2016-03-10 10:26:30 -0800

    added checkCollision, reset prototypes and got a formula almost working for collisions

--- js/app.js
+++ js/app.js
@@ -7,18 +7,19 @@ var Enemy = function(x,y, sprite,speed) {
     // a helper we've provided to easily load images
     this.x = x;
     this.y = y;
-    this.speed=speed;
     this.sprite = 'images/enemy-bug.png';
+    //this.speed=Math.floor((Math.random()*2));
     };
 
     Enemy.prototype.update = function(dt) {
-        
+       //makes it so if it goes off the canvas it reappears 
       
      if (this.x>=505) {
         //ask why it's not really random and they move pretty slowly
-        this.x=Math.random()+(dt*85);
+        this.x=Math.floor(Math.random()+(dt*85));
     }
-    else this.x=this.x+Math.random(500)+(dt*85);
+    else this.x=this.x+Math.floor(Math.random(500)+(dt*85));
+    // console.log("Enemy is "+ this.x,this.y);
      
   };  
 
@@ -35,10 +36,10 @@ var Enemy = function(x,y, sprite,speed) {
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-var enemy1= new Enemy(100,146,200);
-var enemy2 = new Enemy(300,226,205);
-var enemy3 = new Enemy(200,76,208);
-var allEnemies = [enemy1, enemy2,enemy3];
+var enemy1= new Enemy(100,240,600);
+var enemy2 = new Enemy(200,140,400);
+var enemy3 = new Enemy(0,60,250);
+var allEnemies = [enemy1,enemy2,enemy3];
 
 
 // Now write your own player class
@@ -52,18 +53,47 @@ var Player = function(x, y,sprite) {
     this.sprite = 'images/char-cat-girl.png';
     
     };
- 
 
-    Player.prototype.update = function(dt) {
+    Player.prototype.reset = function() {
+        
+            this.x=200;
+            this.y=420;            
+    };
 
-    this.x*dt;
-    this.y*dt;
-    if (this.y<=10){
-        this.x=200;
-        this.y=420;
-    }
+    Player.prototype.checkCollisions = function () {
+        //for (var i=0; i < allEnemies.length; i++){
+            //determine rows enemy1,2,3 can be on
 
+//if (this.x > enemy1.x + 101&&
+   //this.x + 101 > enemy1.x &&
+   //this.y < enemy1.y + 171 &&
+   //171 + this.y > enemy1.y) {
+    // collision detected!
+   //} 
+        
+          if (this.x >= enemy3.x + 0 && 
+        this.x < enemy3.x + 33 &&
+        this.y >= enemy3.y + 0 &&
+        this.y < enemy3.y + 2000) {
+        
+          console.log("Game over!");
+         this.reset();
+      };  
 };
+ 
+ 
+    Player.prototype.update = function() {
+        //collision detection
+        this.checkCollisions();
+     //player wins
+     if (this.y<=20) {
+        console.log("You win sister!");
+        this.reset();
+     }
+    //this.x*dt;
+    //this.y*dt;
+    };
+      
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -80,19 +110,27 @@ Player.prototype.handleInput = function(direction){
         this.x-=100;
     }
     if(direction==='up'&&this.y>=40) {
-        this.y-=85;
+        this.y-=20;
     }
     if(direction==='down'&&this.y<=350) {
         this.y+=85;
     }
+    if (this.y<=0){
+        this.x=200;
+        this.y=420;
+    }
+
+    console.log("Player is " + this.x,this.y);
+
      };
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var player = new Player(200,420); 
   
-        
+ //player.checkCollisions();       
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
@@ -105,5 +143,10 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode])
 });
+
+
+
+
+
 
 
