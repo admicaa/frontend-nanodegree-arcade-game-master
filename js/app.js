a10043bf76beda6dd0ef80bd99e473cca1c5c6bd
topplethepat <lenore.alford@gmail.com>
2016-03-11 14:28:33 -0800

    got enemy speeds different and faster, collision in progress

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 
-var Enemy = function(x,y, sprite,speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    var Enemy = function(x,y,speed) {
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.x = x;
-    this.y = y;
-    this.sprite = 'images/enemy-bug.png';
-    //this.speed=Math.floor((Math.random()*2));
-    };
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/enemy-bug.png';
+        this.speed=speed;
+        };
 
     Enemy.prototype.update = function(dt) {
        //makes it so if it goes off the canvas it reappears 
       
      if (this.x>=505) {
         //ask why it's not really random and they move pretty slowly
-        this.x=Math.floor(Math.random()+(dt*85));
+        this.x=-85;
     }
-    else this.x=this.x+Math.floor(Math.random(500)+(dt*85));
+    else this.x=this.x+(this.speed*dt);
     // console.log("Enemy is "+ this.x,this.y);
      
   };  
@@ -36,9 +36,9 @@ var Enemy = function(x,y, sprite,speed) {
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-var enemy1= new Enemy(100,240,600);
-var enemy2 = new Enemy(200,140,400);
-var enemy3 = new Enemy(0,60,250);
+var enemy1= new Enemy(100,240,400);
+var enemy2 = new Enemy(200,140,300);
+var enemy3 = new Enemy(0,60,205);
 var allEnemies = [enemy1,enemy2,enemy3];
 
 
@@ -57,36 +57,41 @@ var Player = function(x, y,sprite) {
     Player.prototype.reset = function() {
         
             this.x=200;
-            this.y=420;            
+            this.y=430;            
     };
 
     Player.prototype.checkCollisions = function () {
         //for (var i=0; i < allEnemies.length; i++){
             //determine rows enemy1,2,3 can be on
 
-//if (this.x > enemy1.x + 101&&
-   //this.x + 101 > enemy1.x &&
-   //this.y < enemy1.y + 171 &&
+//if (this.x > enemy1.x + 55 &&
+   //this.x + 55 > enemy1.x &&
+   //this.y < enemy1.y + 0 &&
    //171 + this.y > enemy1.y) {
     // collision detected!
    //} 
-        
-          if (this.x >= enemy3.x + 0 && 
-        this.x < enemy3.x + 33 &&
-        this.y >= enemy3.y + 0 &&
-        this.y < enemy3.y + 2000) {
+        //if (this.x===enemy3.x&&this.y===enemy3.y){
+            //debugger;
+        //}
+          if (this.x >= enemy2.x + 6 && 
+        this.x < enemy2.x + 36 &&
+        this.y >= enemy2.y + 6 &&
+        this.y < enemy2.y + 36) 
+            {
         
           console.log("Game over!");
          this.reset();
-      };  
+      }; 
+        
 };
+//};
  
  
     Player.prototype.update = function() {
         //collision detection
         this.checkCollisions();
      //player wins
-     if (this.y<=20) {
+     if (this.y<=10) {
         console.log("You win sister!");
         this.reset();
      }
@@ -110,25 +115,23 @@ Player.prototype.handleInput = function(direction){
         this.x-=100;
     }
     if(direction==='up'&&this.y>=40) {
-        this.y-=20;
+        this.y-=60;
     }
     if(direction==='down'&&this.y<=350) {
         this.y+=85;
     }
-    if (this.y<=0){
+    if (this.y<=5){
         this.x=200;
-        this.y=420;
+        this.y=430;
     }
 
     console.log("Player is " + this.x,this.y);
 
      };
-
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var player = new Player(200,420); 
+var player = new Player(200,430); 
   
  //player.checkCollisions();       
 // This listens for key presses and sends the keys to your
